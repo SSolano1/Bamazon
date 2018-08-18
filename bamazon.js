3a5f12eec32d12b721875fad0ba135e429240db9
Solon Solano <ssolano1@msn.com>
2018-08-18 08:59:39 -0400

    "initial"

---
+++ bamazon.js
@@ -0,0 +1,15 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 8889,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "root",
+  database: "bamazonDB"
+});
