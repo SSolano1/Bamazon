4a3670449342e827e9d17ecd75f7c7217e6ef458
Solon Solano <ssolano1@msn.com>
2018-08-18 17:02:14 -0400

    added user input

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,6 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
+const cTable = require("console.table");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -11,7 +13,44 @@ var connection = mysql.createConnection({
 
   // Your password
   password: "root",
-  database: "bamazonDB"
+
+  database: "bamazon"
 });
 
+// make the connection
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+
+  displayItems();
+
+});  
+
+// prompts the customer for an action
+function displayItems() {
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      console.table(res);
+
+    // Prompt for the input from the user
+      inquirer.prompt ([
+        {
+        type: "input",
+        name: "id",
+        message: "Enter the ID of the the product you want to purchase?"
+        },
+        {
+        type: "input",
+        name: "quantity",
+        message: "How many would you like to purchase?"  
+        }
+      ])
+
+    });
+    
+}
   
+
+
+  
+
--- schema.sql
+++ schema.sql
@@ -7,8 +7,8 @@ CREATE TABLE products(
   id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(100) NOT NULL,
   department_name VARCHAR(45) NOT NULL,
-  price INT (10),
-  stock_quantity INT (10),
+  price DECIMAL(15,2) NULL,
+  stock_quantity INT (10) NULL,
   PRIMARY KEY (id)
 );
 
